443d53fb639e52fc741a84ebe8bfd25069f07545
vocaoson <vocaoson7@gmail.com>
2018-10-20 22:22:20 +0700

    Nothing

--- Test/test/KingServlet.java
+++ Test/test/KingServlet.java
@@ -21,7 +21,7 @@ public class KingServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		req.getRequestDispatcher("/admin/starter.jsp").forward(req, resp);
+		req.getRequestDispatcher("/admin/khang.jsp").forward(req, resp);
 	}
 
 }
